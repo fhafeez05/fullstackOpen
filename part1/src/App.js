0bc7fe2222cb7bb0a0684666f1ad4205db8d50a9
fhafeez05 <78035687+fhafeez05@users.noreply.github.com>
2022-04-18 17:16:35 -0400

    finished part 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,70 @@
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
+  return (
+    <div>
+      <h1> Greetings </h1>
+      <Header course={course}/>
+      <p>
+        <Content part1={part1} exercises1={exercises1}/>
+      </p>
+      <p>
+        <Content part2={part2} exercises2={exercises2}/>
+     </p>
+      <p>
+        <Content part3={part3} exercises3={exercises3}/>
+      </p>
+      <p>
+        <Total exercises1={exercises1 + exercises2 +exercises3 }/> 
+        {/* Number of exercises {exercises1 + exercises2 + exercises3} */}
+        </p>
+     
+
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+    <p>This is {props.course}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+    <p>Number of excercises {props.exercises1} {props.exercises} {props.exercises3}</p>
+    </div>
+  )
+}
 
 
+export default App
+
+
+
+
+
+
+/*
 //props can be used to send variables to other 'classes' - the App environment is the highest priority class in the hierarchy 
 const Hello = (props) => {
   return (
@@ -22,6 +86,4 @@ const App = () => {
     </div>
   )
   }  
-
-
-export default App;
+*/
